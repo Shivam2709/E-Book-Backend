d93ae82e52f8d054bbc38f5eadce4f0c91c9e4aa
Shivam Thakur <shivamthakur2709@gmail.com>
2024-06-11 14:16:26 +0530

    setup express

--- server.ts
+++ server.ts
@@ -1,1 +1,13 @@
-console.log("Welcome to ebook Apis");
+import app from "./src/app";
+
+
+const startServer = () => {
+    const port = process.env.PORT || 5000;
+
+    app.listen(port, () => {
+        console.log(`Listening on port: ${port}`);
+        
+    })
+}
+
+startServer();
