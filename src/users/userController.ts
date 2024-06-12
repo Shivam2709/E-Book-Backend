ef37ef4f023f0e3893cda7eacf414d29761a4f13
Shivam Thakur <shivamthakur2709@gmail.com>
2024-06-12 17:20:11 +0530

    complete login endPoint

--- src/users/userController.ts
+++ src/users/userController.ts
@@ -1,52 +1,97 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel";
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
 import { sign } from "jsonwebtoken";
 import { config } from "../config/config";
+import { User } from "./userTypes";
 
-const createUser = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-    const { name, email, password } = req.body;
+const createUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { name, email, password } = req.body;
 
-    // validation
-    if (!name || !email || !password) {
-        const error = createHttpError(400, "All fields are required");
-        return next(error);
-    }
+  // validation
+  if (!name || !email || !password) {
+    const error = createHttpError(400, "All fields are required");
+    return next(error);
+  }
 
-    // database call
-    const user = await userModel.findOne({email});
-
-    if(user) {
-      const error = createHttpError(400, "user already exists with this email.");
+  // database call
+  try {
+    const user = await userModel.findOne({ email });
+    if (user) {
+      const error = createHttpError(
+        400,
+        "user already exists with this email."
+      );
       return next(error);
     }
+  } catch (err) {
+    return next(createHttpError(500, "Error while getting user."));
+  }
+  // process
+  // password --> hash
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-    // password --> hash
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const newUser = await userModel.create({
+  let newUser: User;
+  try {
+    newUser = await userModel.create({
       name,
-      email, 
+      email,
       password: hashedPassword,
-    })
+    });
+  } catch (err) {
+    return next(createHttpError(500, "Error while creating user."));
+  }
 
-    // Token generation JWT
-    const token = sign({sub: newUser._id}, config.jwtSecret as string, {
-      expiresIn: '7d',
+  // Token generation JWT
+  try {
+    const token = sign({ sub: newUser._id }, config.jwtSecret as string, {
+      expiresIn: "7d",
       algorithm: "HS256",
     });
 
-    // process
-
     // Response
-    res.json({ accessToken: token });
-    
-    
+    res.status(201).json({ accessToken: token });
+  } catch (err) {
+    return next(createHttpError(500, "Error while signing the jwt token."));
+  }
 };
 
-export { createUser };
+const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(createHttpError(400, "Please provide email and password"));
+  }
+
+  let user;
+  try {
+    user = await userModel.findOne({ email });
+
+    if (!user) {
+      return next(createHttpError(404, "User not found."));
+    }
+  } catch (err) {
+    return next(createHttpError(500, "Error while getting user."));
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password as string);
+
+  if (!isMatch) {
+    return next(createHttpError(401, "Username or password incorrect."));
+  }
+
+  // create accessToken
+  try {
+    const token = sign({ sub: user._id }, config.jwtSecret as string, {
+      expiresIn: "7d",
+      algorithm: "HS256",
+    });
+
+    res.status(201).json({ accessToken: token });
+  } catch (err) {
+    return next(createHttpError(500, "Error while signing the jwt token."));
+  }
+};
+
+export { createUser, loginUser };
