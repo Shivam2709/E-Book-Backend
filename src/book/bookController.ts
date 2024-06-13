9048f062675b177e993e0220de1d430ab62beeed
Shivam Thakur <shivamthakur2709@gmail.com>
2024-06-13 16:58:07 +0530

    Uploade file to cloudinary

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,7 +1,56 @@
+import path from "node:path";
 import { Request, Response, NextFunction } from "express";
+import cloudinary from "../config/cloudinary";
+import createHttpError from "http-errors";
 
-const createBook = async(req: Request, res: Response, next: NextFunction) => {
+const createBook = async (req: Request, res: Response, next: NextFunction) => {
+  // console.log('Files', req.files);
+
+  // upload coverImage in Cloudinary using multer
+  const files = req.files as { [fieldname: string]: Express.Multer.File[] }; // for type in the typeScript for file data from milter.
+  // MimeTypeArray = application/pdf
+  const coverImageMimeType = files.coverImage[0].mimetype.split("/").at(-1);
+  const fileName = files.coverImage[0].filename;
+  const filePath = path.resolve(
+    __dirname,
+    "../../public/data/uploads",
+    fileName
+  );
+
+  try {
+    const uploadResult = await cloudinary.uploader.upload(filePath, {
+      filename_override: fileName,
+      folder: "book-covers",
+      format: coverImageMimeType,
+    });
+
+    // upload book in Cloudinary using multer
+    const bookFileName = files.file[0].filename;
+    const bookFilePath = path.resolve(
+      __dirname,
+      "../../public/data/uploads",
+      bookFileName
+    );
+
+    const bookFileUploaderResult = await cloudinary.uploader.upload(
+      bookFilePath,
+      {
+        resource_type: "raw",
+        filename_override: bookFileName,
+        folder: "book-pdfs",
+        format: "pdf",
+      }
+    );
+
+    // console.log("Book details", bookFileUploaderResult);
+
+    // console.log('Upload result', uploadResult);
+
     res.json({});
-}
+  } catch (err) {
+    console.log(err);
+    return next(createHttpError(500, "Error while uploading the files."));
+  }
+};
 
 export { createBook };
--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,26 +1,24 @@
-import path from 'node:path';
+import path from "node:path";
 import express from "express";
-import { createBook } from './bookController';
-import multer from 'multer';
-
+import { createBook } from "./bookController";
+import multer from "multer";
 
 const bookRouter = express.Router();
 
 // flie store in local --> then move to cloudnary
 const upload = multer({
-    dest: path.resolve(__dirname, '../../public/data/uploads'),
-    limits: { fileSize: 3e7} // 3e7 means = 30mb = 30 * 1024 * 1024...
-})
+  dest: path.resolve(__dirname, "../../public/data/uploads"),
+  limits: { fileSize: 3e7 }, // 3e7 means = 30mb = 30 * 1024 * 1024...
+});
 
 // Routes
 bookRouter.post(
-    "/", 
-    upload.fields([
-        {name: 'coverImage', maxCount: 1},
-        {name: 'file', maxCount: 1},
-    ]), 
-    createBook
+  "/",
+  upload.fields([
+    { name: "coverImage", maxCount: 1 },
+    { name: "file", maxCount: 1 },
+  ]),
+  createBook
 );
-
 
 export default bookRouter;
