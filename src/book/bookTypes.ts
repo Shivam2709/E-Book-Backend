2ff24150bc4e2ff2ab86e8a4d7e8d217c5f4fcf4
Shivam Thakur <shivamthakur2709@gmail.com>
2024-06-13 14:49:36 +0530

    setup multer and book section

---
+++ src/book/bookTypes.ts
@@ -0,0 +1,10 @@
+export interface Book {
+    _id: String;
+    title: String;
+    author: String;
+    genre: String;
+    coverImage: string;
+    file: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
